c7f2877ae681d348b8bdd5e7c26760289fac6470
jacobwmorris <jacobwmorris@gmail.com>
2022-09-14 13:36:34 -0400

    Add play round function

--- rps.js
+++ rps.js
@@ -13,26 +13,55 @@ function getComputerChoice() {
     }
 }
 
-let rockCt = 0;
-let paperCt = 0;
-let scissorsCt = 0;
-const iterations = 500;
+function playRound(playerSelection, compSelection) {
+    let victory = false;
+    playerSelection = playerSelection.toLowerCase();
 
-for (let i = 0; i < iterations; ++i) {
-    let choice = getComputerChoice();
-
-    if (choice === "rock") {
-        ++rockCt;
+    //Check for a tie first
+    if (playerSelection === compSelection) {
+        return `It's a tie! you both chose ${playerSelection}.`;
     }
-    else if (choice === "paper") {
-        ++paperCt;
+
+    //Rock beats scissors, paper beats rock, scissors beats paper
+    if (playerSelection === "rock") {
+        if (compSelection === "paper") {
+            victory = false;
+        }
+        else if (compSelection === "scissors") {
+            victory = true;
+        }
+    }
+    else if (playerSelection === "paper") {
+        if (compSelection === "rock") {
+            victory = true;
+        }
+        else if (compSelection === "scissors") {
+            victory = false;
+        }
     }
     else {
-        ++scissorsCt;
+        if (compSelection === "rock") {
+            victory = false;
+        }
+        else if (compSelection === "paper") {
+            victory = true;
+        }
+    }
+
+    if (victory) {
+        return `You win! ${playerSelection} beats ${compSelection}.`
+    }
+    else {
+        return `You lose! ${compSelection} beats ${playerSelection}.`
     }
 }
 
-let rockPct = rockCt / iterations * 100;
-let paperPct = paperCt / iterations * 100;
-let scissorsPct = scissorsCt / iterations * 100;
-console.log(`Results: ${rockPct}% rock, ${paperPct}% paper, ${scissorsPct}% scissors.`);
+console.log(playRound("rock", "rock"));
+console.log(playRound("rock", "paper"));
+console.log(playRound("rock", "scissors"));
+console.log(playRound("paper", "rock"));
+console.log(playRound("paper", "paper"));
+console.log(playRound("paper", "scissors"));
+console.log(playRound("scissors", "rock"));
+console.log(playRound("scissors", "paper"));
+console.log(playRound("scissors", "scissors"));
